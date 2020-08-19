21961c43f2bd0f9e9907bc9a172f5c62bb8c0846
tbonza <tylers.pile@gmail.com>
2020-08-19 14:00:06 -0400

    added slower julia benchmark

--- regex_slower_benchmark.jl
+++ regex_slower_benchmark.jl
@@ -1,6 +1,9 @@
-using Umbrella
-
 regexes = [Regex(i) for i in readlines("yelp_example.regex")]
 text = readlines("yelp_example.txt")
 
-example1(text, regexes)
+for t in text
+    for r in regexes
+        if occursin(r,t)
+        end
+    end
+end
